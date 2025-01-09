865632f842fc329d5e565de869ee895889a62294
Sumalatha-Takkoli <takkoli.sumalatha@gmail.com>
2025-01-09 22:52:34 +0530

    basic structure for players(static)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-
 function App() {
-  
-
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+return (
+<main>
+  <div id="game-container">
+    <ol id="players">
+      <li>
+        <span className="player">
+        <span className="player-name">player1</span>
+        <span className="player-symbol">X</span>
+        </span>
+      </li>
+      <li>
+        <span className="player">
+        <span className="player-name">player2</span>
+        <span className="player-symbol">O</span>
+        </span>
+      </li>
+    </ol>
+    GAMEBOARD
+  </div>
+  LOG
+</main>
+)
 }
 
 export default App
